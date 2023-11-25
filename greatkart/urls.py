43891252c14615be3e8e64ad0060405932255216
bkocii <burimkoci@hotmail.com>
2023-11-25 09:29:57 +0100

    store and carts functionality

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -19,11 +19,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-import store
+import store, carts
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
