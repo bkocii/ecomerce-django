4dc0d320025b983212baf004588349c55267a606
bkocii <burimkoci@hotmail.com>
2023-12-11 23:37:59 +0100

    created orders and payments

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,5 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
